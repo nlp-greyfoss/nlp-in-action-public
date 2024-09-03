0db806c504dd33be11dc7b9e1aa0929593a81737
nlp-greyfoss <jueying.szu@gmail.com>
2024-09-03 13:04:29 +0800

    sbert-mse

--- text_matching/sentence-berts/bi-encoder/sentence-bert/test.sh
+++ text_matching/sentence-berts/bi-encoder/sentence-bert/test.sh
@@ -1,6 +1,6 @@
 # change CUDA_VISIBLE_DEVICES
 CUDA_VISIBLE_DEVICES=0 python test.py \
     --model_name_or_path=<output-of-train.py> \
-    --test_data_path=data/test.xt
+    --test_data_path=data/test.txt
    
 
